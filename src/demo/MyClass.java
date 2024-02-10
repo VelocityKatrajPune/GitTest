629b16f9d854f8ae18fc7a34beda7af3d18bc762
latika12j <latika12us@gmail.com>
2024-02-10 20:32:41 +0530

    Test case added

--- src/demo/MyClass.java
+++ src/demo/MyClass.java
@@ -5,6 +5,8 @@ public class MyClass {
 	public static void main(String[] args) {
 		
 		System.out.println("HI Hello");
+		System.out.println("Changes Added 1");
+		System.out.println("Changes Added 2");
 
 	}
 
